b1d1350b300b61fab5a3d37c9f0ff2ce61cd3997
GyowonS <152247242+GyowonS@users.noreply.github.com>
2024-02-12 19:34:50 +0900

    Update ForgotPW.js

--- login/android/app/Screens/ForgotPW.js
+++ login/android/app/Screens/ForgotPW.js
@@ -245,124 +245,196 @@ const handleClick = () => {
   );
 } 
 
-  const styles = StyleSheet.create({
-    inputP: {
-      fontSize: 15,
-      borderBottomWidth: 0.5,
-      height: 40,
-      width: 232,
-      top: 80,
-      marginBottom: 20,
-      color: '#878787',
-    },
-    buttonP: {
-      top: 100,
-      backgroundColor: '#FEA655',
-      paddingVertical: 10,
-      paddingHorizontal: 48,
-      borderRadius: 25,
-    },
-    midButton: {
-      right: 48,
-      top: 64,
-      backgroundColor: '#FEA655',
-      paddingVertical: 10,
-      paddingHorizontal: 23,
-      borderRadius: 25,
-    },
-    resend: {
-      top: 33,
-      left: 75,
-    },
-    inputRe: {
-      fontSize: 15,
-      borderBottomWidth: 0.5,
-      height: 40,
-      width: 160,
-      top: 82,
-      right: 35,
-      marginBottom: 10,
-      color: '#878787',
-    },
-    smallButton: {
-      left: 85,
-      top: 36,
-      backgroundColor: '#FEA655',
-      paddingVertical: 10,
-      paddingHorizontal: 18,
-      borderRadius: 25,
-    },
-    passwordContent: {
-      top: 50,
-      fontSize: 12,
-      color: '#ff0000',
-    },
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      backgroundColor: '#F8F9FA', // 배경색상 추가
-    },
-    topTitle: {
-      fontSize: 24,
-      top: 12,
-      marginLeft:-100
-    },
-    input: {
-      fontSize: 15,
-      borderWidth: 0.5,
-      height: 28,
-      width: 232,
-      marginBottom: 30,
-      color: '#878787',
-      borderTopWidth: 0,
-      borderLeftWidth: 0,
-      borderRightWidth:0,
-    },
-    save: {
-      flexDirection: 'row', 
-      justifyContent: 'space-evenly',
-      bottom: 15,
-      right: 45,
-    },
-    saveTxt: {
-      fontSize: 12,
-      color: '#757575',
-    },
-    signUp: {
-      top: 105,
-      right: 100,
-      paddingHorizontal: 100,
-    },
-    reset: {
-      top: 90,
-      left: 100,
-      paddingHorizontal: 100,
-    },
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#F8F9FA', // 배경색상 추가
+  },
+  title: {
+    fontSize: 40,
+    top: 47,
+    marginBottom: 211,
+    fontFamily: 'NanumGothic',
+    color: '#000', // 글자색상 추가
+  },
+
+  input: {
+    fontSize: 15,
+    borderWidth: 0.5,
+    height: 28,
+    width: 232,
+    marginBottom: 30,
+    color: '#878787',
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth:0,
+    paddingVertical: 0
+  },
+  save: {
+    position: 'absolute',
+    left: 85,
+    top: 375,
+    width: 15,
+    height: 15, 
+    borderRadius: 25,
+    marginBottom: 20,
+  },
+  saveTxt: {
+    fontSize: 12,
+    color: '#757575',
+  },
+  signUp: {
+    top: 105,
+    right: 100,
+    paddingHorizontal: 100,
+  },
+  reset: {
+    top: 90,
+    left: 100,
+    paddingHorizontal: 100,
+  },
+
+  button: {
+    top: 85,
+    backgroundColor: '#FEA655',
+    paddingVertical: 10,
+    paddingHorizontal: 48,
+    borderRadius: 25,
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 15,
+    fontWeight: 'bold',
+    fontFamily: 'NanumGothic',
+  },
+
+  //budgetpg
+  introBudget: {
+    fontSize: 20,
+    top: 149,
+    marginBottom: 211,
+    fontFamily: 'NanumGothic',
+    color: '#000', // 글자색상 추가
+  },
+  subTitle: {
+    color: '#727272',
+    bottom: 45,
+  },
+    back: {
+    top: 42,
+    right: 163,
+  },
+  backBTN: {
+    fontSize: 25,
+  },
+
+  //signuppg
+  topTitle: {
+    fontSize: 24,
+    top: 12,
+  },
+  log: {
+    top: 193,
+
+  },
+  inputS: {
+    fontSize: 15,
+    borderWidth: 0.5,
+    height: 28,
+    width: 232,
+    top: 130,
+    marginBottom: 40,
+    color: '#878787',
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth:0,
+    paddingVertical: 0
+  },
+  buttonS: {
+    top: 183,
+    backgroundColor: '#FEA655',
+    paddingVertical: 10,
+    paddingHorizontal: 48,
+    borderRadius: 25,
+    marginBottom: 20,
+  },
   
-    button: {
-      top: 85,
-      backgroundColor: '#FEA655',
-      paddingVertical: 10,
-      paddingHorizontal: 48,
-      borderRadius: 25,
-      marginBottom: 20,
-    },
-    buttonText: {
-      color: '#fff',
-      fontSize: 15,
-      fontWeight: 'bold',
-      fontFamily: 'NanumGothic',
-    },
-    backBTN: {
-      fontSize: 25,
-      marginLeft:-140,
-      marginTop:11,
-    },
-    errTxt: {
+  //success
+  center: {
+    fontSize: 20,
+    top: 20,
+    marginBottom: 55,
+    textAlign: 'center',
+  },
+
+  //forgotPW
+  inputP: {
+    fontSize: 15,
+    borderWidth: 0.5,
+    height: 28,
+    width: 232,
+    top: 80,
+    marginBottom: 40,
+    color: '#878787',
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth:0,
+    paddingVertical: 0
+  },
+  buttonP: {
+    top: 93,
+    backgroundColor: '#FEA655',
+    paddingVertical: 10,
+    paddingHorizontal: 48,
+    borderRadius: 25,
+  },
+  midButton: {
+    right: 48,
+    top: 64,
+    backgroundColor: '#FEA655',
+    paddingVertical: 10,
+    paddingHorizontal: 23,
+    borderRadius: 25,
+  },
+  resend: {
+    top: 33,
+    left: 75,
+  },
+  inputRe: {
+    fontSize: 15,
+    borderWidth: 0.5,
+    height: 28,
+    width: 160,
+    top: 82,
+    right: 35,
+    marginBottom: 10,
+    color: '#878787',
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth:0,
+    paddingVertical: 0
+  },
+  smallButton: {
+    left: 85,
+    top: 36,
+    backgroundColor: '#FEA655',
+    paddingVertical: 10,
+    paddingHorizontal: 18,
+    borderRadius: 25,
+  },
+  passwordContent: {
+    top: 50,
+    fontSize: 12,
+    color: '#ff0000',
+  },
+  errTxt: {
     top: 100,
     fontSize: 12,
     color: '#ff0000',
   },
-  })
+});
   
   export default ForgotPW;
