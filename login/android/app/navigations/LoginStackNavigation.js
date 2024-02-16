ca765b208116813b567740786c1e9f8ed6e1f9f7
GyowonS <152247242+GyowonS@users.noreply.github.com>
2024-02-16 20:04:45 +0900

    이용약관 페이지 추가함

---
+++ login/android/app/navigations/LoginStackNavigation.js
@@ -0,0 +1,27 @@
+import Loginpg from "../Screens/Loginpg";
+import SignupAgree from "../Screens/SignupAgree";
+import SignupPg from "../Screens/SignupPg";
+import ForgotPW from "../Screens/ForgotPW";
+import SuccessPW from "../Screens/SuccessPW";
+import SuccessLogin from "../Screens/SuccessLogin";
+import Budgetpg from "../Screens/Budgetpg";
+
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const LoginStackNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="LoginpgStack" component={Loginpg} />
+      <Stack.Screen name="SignupAgree" component={SignupAgree} />
+      <Stack.Screen name="SignupPg" component={SignupPg} />
+      <Stack.Screen name="ForgotPW" component={ForgotPW} />
+      <Stack.Screen name="SuccessPW" component={SuccessPW} />
+      <Stack.Screen name="SuccessLogin" component={SuccessLogin} />
+      <Stack.Screen name="Budgetpg" component={Budgetpg} />
+    </Stack.Navigator>
+  )
+};
+
+export default LoginStackNavigator;
