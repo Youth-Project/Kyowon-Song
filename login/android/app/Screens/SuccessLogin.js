9ae97769ffa07e65c2411ce55ffa68223d5a58d0
GyowonS <152247242+GyowonS@users.noreply.github.com>
2024-02-16 17:24:31 +0900

    Update SuccessLogin.js

--- login/android/app/Screens/SuccessLogin.js
+++ login/android/app/Screens/SuccessLogin.js
@@ -19,7 +19,7 @@ const SuccessLogin = ({navigation}) => {
       <Text style={styles.center}>축하드립니다 !{"\n"}회원가입이 완료되었습니다.</Text>
       <TouchableOpacity
         style={styles.button}
-        onPress={() => navigation.navigate('Main')}>
+        onPress={() => navigation.navigate('Budgetpg')}>
         <Text style={styles.buttonText}>로그인</Text>
       </TouchableOpacity>
       
