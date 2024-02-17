b697da20a68ee25dfcbeb5f82b439ff6cc59bb37
GyowonS <152247242+GyowonS@users.noreply.github.com>
2024-02-17 19:50:33 +0900

    Update SignupPg.js

--- login/android/app/Screens/SignupPg.js
+++ login/android/app/Screens/SignupPg.js
@@ -232,7 +232,7 @@ const styles = StyleSheet.create({
   input: {
     fontSize: 15,
     borderWidth: 0.5,
-    height: 28,
+    height: 38,
     width: 232,
     marginBottom: 30,
     color: '#878787',
@@ -244,7 +244,7 @@ const styles = StyleSheet.create({
   save: {
     position: 'absolute',
     left: 85,
-    top: 375,
+    top: 395,
     width: 15,
     height: 15, 
     borderRadius: 25,
@@ -312,7 +312,7 @@ const styles = StyleSheet.create({
   inputS: {
     fontSize: 15,
     borderWidth: 0.5,
-    height: 28,
+    height: 38,
     width: 232,
     top: 130,
     marginBottom: 40,
@@ -343,7 +343,7 @@ const styles = StyleSheet.create({
   inputP: {
     fontSize: 15,
     borderWidth: 0.5,
-    height: 28,
+    height: 38,
     width: 232,
     top: 80,
     marginBottom: 40,
@@ -375,7 +375,7 @@ const styles = StyleSheet.create({
   inputRe: {
     fontSize: 15,
     borderWidth: 0.5,
-    height: 28,
+    height: 38,
     width: 160,
     top: 82,
     right: 35,
