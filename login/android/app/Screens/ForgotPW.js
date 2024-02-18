eea56cbf394c5b26f9679c22d5734363e09f73bb
GyowonS <152247242+GyowonS@users.noreply.github.com>
2024-02-18 19:35:37 +0900

    Update ForgotPW.js

--- login/android/app/Screens/ForgotPW.js
+++ login/android/app/Screens/ForgotPW.js
@@ -336,7 +336,7 @@ const styles = StyleSheet.create({
     borderTopWidth: 0,
     borderLeftWidth: 0,
     borderRightWidth:0,
-    paddingVertical: 0
+    paddingTop: 7,
   },
   save: {
     position: 'absolute',
@@ -417,7 +417,7 @@ const styles = StyleSheet.create({
     borderTopWidth: 0,
     borderLeftWidth: 0,
     borderRightWidth:0,
-    paddingVertical: 0
+    paddingTop: 7,
   },
   buttonS: {
     top: 183,
@@ -448,7 +448,7 @@ const styles = StyleSheet.create({
     borderTopWidth: 0,
     borderLeftWidth: 0,
     borderRightWidth:0,
-    paddingVertical: 0
+    paddingTop: 7,
   },
   buttonP: {
     top: 93,
@@ -481,7 +481,7 @@ const styles = StyleSheet.create({
     borderTopWidth: 0,
     borderLeftWidth: 0,
     borderRightWidth:0,
-    paddingVertical: 0
+    paddingTop: 7,
   },
   smallButton: {
     left: 85,
